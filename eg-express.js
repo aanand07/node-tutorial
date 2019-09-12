b117ea92031bd52d61f03d82bd84077a12563464
aanand07 <bhushananand007@gmail.com>
2019-09-12 23:29:09 +0530

    passport authentication

--- eg-express.js
+++ eg-express.js
@@ -4,26 +4,57 @@ const morgan = require('morgan');
 const dishRouter = require('./routes/dishRouter');
 const promotionRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const usersRouter = require('./routes/userRouter');
 const mongoose = require('mongoose');
+var passport = require('passport');
+var authenticate = require('./authenticate');
+// const cookieParser = require('cookie-parser');
+
 
 const host = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/static-html'));
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+// app.use(session({
+//     name: 'session-id',
+//     secret: '12345-67890-09876-54321',
+//     saveUninitialized: false,
+//     resave: false,
+//     store: new FileStore()
+// }));
+
+app.use(passport.initialize());
+// app.use(passport.session());
+
+function auth (req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      next(err);
+    }
+    else {
+          next();
+    }
+}
 
 connect.then(() => {
     console.log("Connected correctly to server");
 }, (err) => { console.log(err); });
 
+// app.use(auth);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
-
+app.use('/users', usersRouter);
+app.use(express.static(__dirname + '/static-html'));
 // for invalid requests
 app.use((req, res, next) => {
     res.statusCode = 200;
---
+++ models/user.js
@@ -0,0 +1,14 @@
+var passportLocalMongoose = require('passport-local-mongoose');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
